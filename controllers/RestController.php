4c3f7b2dce7eeefb5f64b30d2ab8053f789a7bd1
Tomek Świerszcz <tomek.swierszcz@gmail.com>
2019-05-23 17:06:41 +0200

    CorsCustom included

--- controllers/RestController.php
+++ controllers/RestController.php
@@ -5,7 +5,7 @@ namespace backend\controllers;
 use Yii;
 use yii\web\Controller;
 use app\models\LoginForm;
-//use common\helpers\CorsCustom;
+use common\helpers\CorsCustom;
 
 
 class RestController extends Controller
