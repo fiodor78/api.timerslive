c57c3fb054406d55485c4cecf308e3217c659d05
Tomek Świerszcz <tomek.swierszcz@gmail.com>
2019-05-23 15:04:52 +0200

    Access-Control-Allow-Credentials to true

--- controllers/RestController.php
+++ controllers/RestController.php
@@ -29,7 +29,7 @@ class RestController extends Controller
                 'Access-Control-Allow-Origin' => ['*'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['*'],
-                'Access-Control-Allow-Credentials' => null,
+                'Access-Control-Allow-Credentials' => true,
                 'Access-Control-Max-Age' => 86400,
                 'Access-Control-Expose-Headers' => []
             ]
